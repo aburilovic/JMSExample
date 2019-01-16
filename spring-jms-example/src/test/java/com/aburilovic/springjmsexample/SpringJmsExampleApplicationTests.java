738f4fa935612bccc6472725da44c2268e081394
Ante Burilovic <ante.burilovic.dev@gmail.com>
2019-01-16 13:12:58 +0100

    Added Sender and Receiver with respective configs

--- spring-jms-example/src/test/java/com/aburilovic/springjmsexample/SpringJmsExampleApplicationTests.java
+++ spring-jms-example/src/test/java/com/aburilovic/springjmsexample/SpringJmsExampleApplicationTests.java
@@ -1,17 +1,33 @@
 package com.aburilovic.springjmsexample;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.aburilovic.springjmsexample.jms.Receiver;
+import com.aburilovic.springjmsexample.jms.Sender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringJmsExampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private Sender sender;
 
+	@Autowired
+	private Receiver receiver;
+
+	@Test
+	public void testReceive() throws Exception {
+		sender.send("Hello Spring JMS ActiveMQ!");
+
+		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		assertThat(receiver.getLatch().getCount()).isEqualTo(0);
+	}
 }
 
